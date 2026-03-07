97d1d35841bcf41676a78da345b632ff675aaed5
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2026-03-07 11:28:17 +0530

    [Ashish]: added contact pojo class

--- src/main/java/com/bridgelabz/Contact.java
+++ src/main/java/com/bridgelabz/Contact.java
@@ -91,4 +91,11 @@ public class Contact {
     public void setEmail(String email) {
         this.email = email;
     }
+    
+    
+    @Override
+	public String toString() {
+		return "First Name: " + firstName + "\nLast Name: " + lastName + "\nAddress: " + address + "\nCity: " + city
+				+ "\nState: " + state + "\nZip: " + zip + "\nPhone Number: " + phoneNumber + "\nEmail: " + email;
+	}
 }
