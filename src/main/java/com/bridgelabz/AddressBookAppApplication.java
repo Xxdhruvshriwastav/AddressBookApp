fbd76e58dbd6e9a9da696bd66d979e5f008b8687
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2026-03-07 12:25:16 +0530

    [Ashish]: added AddressBookAppApplication

--- src/main/java/com/bridgelabz/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/AddressBookAppApplication.java
@@ -8,30 +8,40 @@ public class AddressBookAppApplication {
 	
 	
 	public static Contact addContact() {
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter details:- ");
-		System.out.print("First name: ");
-		String firstName = sc.nextLine();
-		System.out.print("Last name: ");
-		String lastName = sc.nextLine();
-		System.out.print("Address: ");
-		String address = sc.nextLine();
-		System.out.print("City: ");
-		String city = sc.nextLine();
-		System.out.print("State: ");
-		String state = sc.nextLine();
-		System.out.print("Zip code: ");
-		int zip = sc.nextInt();
-		System.out.println("Phone number: ");
-		int phoneNumber = sc.nextInt();
-		sc.nextLine();
-		System.out.println("Email: ");
-		String email = sc.nextLine();
-		
-		Contact c = new Contact(firstName, lastName, address, city, state, city, state, email);
-		return c;
-		
+
+	    Scanner sc = new Scanner(System.in);
+
+	    System.out.println("Enter Contact Details");
+
+	    System.out.print("First Name: ");
+	    String firstName = sc.nextLine();
+
+	    System.out.print("Last Name: ");
+	    String lastName = sc.nextLine();
+
+	    System.out.print("Address: ");
+	    String address = sc.nextLine();
+
+	    System.out.print("City: ");
+	    String city = sc.nextLine();
+
+	    System.out.print("State: ");
+	    String state = sc.nextLine();
+
+	    System.out.print("Zip: ");
+	    String zip = sc.nextLine();
+
+	    System.out.print("Phone Number: ");
+	    String phoneNumber = sc.nextLine();
+
+	    System.out.print("Email: ");
+	    String email = sc.nextLine();
+
+	    Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
+	    System.out.println("Contact Added Successfully!");
+
+	    return contact;
 	}
 	
 	
