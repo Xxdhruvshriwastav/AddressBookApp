b2c580dfda06407d0ff8175288e554ef3e5a0a3e
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2026-03-07 15:38:31 +0530

    [Ashish]: added program of Prevent-Duplicate-Contact

--- src/main/java/com/bridgelabz/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/AddressBookAppApplication.java
@@ -208,7 +208,20 @@ public class AddressBookAppApplication {
         
         // UC-7
         
-        
+        public void addPerson(ArrayList<Contact> addressBook, Contact person) {
 
+            boolean duplicate = addressBook.stream()
+                    .anyMatch(p -> p.getFirstName().equalsIgnoreCase(person.getFirstName())
+                            && p.getLastName().equalsIgnoreCase(person.getLastName()));
+
+            if (duplicate) {
+                System.out.println("Duplicate entry! Person already exists.");
+            } else {
+                addressBook.add(person);
+                System.out.println("Person added successfully.");
+            }
+       
+
+            
     }
 }
