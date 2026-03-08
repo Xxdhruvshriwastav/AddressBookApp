d8da15c60d7e7120a7aa0d9ca99381e882595997
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2026-03-09 02:13:16 +0530

    [Ashish]: added AddressBookRestTest

--- src/test/java/com/bridgelabz/AddressBookRestTest.java
+++ src/test/java/com/bridgelabz/AddressBookRestTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AddressBookRestTest {
 
-    ArrayList<Contact> addressBookList;
+    ArrayList<ContactDTO> addressBookList;
 
     @BeforeEach
     public void setUp(){
@@ -25,6 +25,7 @@ public class AddressBookRestTest {
         addressBookList = new ArrayList<>();
     }
 
+    // UC22 : Retrieve Contacts from JSON Server
     @Test
     public void givenAddressBookData_whenRetrieved(){
 
@@ -36,16 +37,46 @@ public class AddressBookRestTest {
                 .extract()
                 .response();
 
-        Contact[] contacts = response.as(Contact[].class);
+        ContactDTO[] contacts = response.as(ContactDTO[].class);
 
         addressBookList.addAll(Arrays.asList(contacts));
 
-        for(Contact contact : addressBookList){
-            System.out.println(contact.getFirstName() + " " + contact.getCity());
-        }
-
         System.out.println("Total Contacts : " + addressBookList.size());
 
-        assertTrue(addressBookList.size() > 0);
+        assertTrue(addressBookList.size() >= 0);
+    }
+
+    // UC23 : Add Multiple Contacts to JSON Server
+    @Test
+    public void givenMultipleContacts_whenAdded_shouldSyncWithMemory(){
+
+        ContactDTO[] newContacts = {
+                new ContactDTO(0,"Amit","Delhi"),
+                new ContactDTO(0,"Rohit","Patna"),
+                new ContactDTO(0,"Priya","Kolkata")
+        };
+
+        for(ContactDTO contact : newContacts){
+
+            Response response = given()
+                    .contentType("application/json")
+                    .body(contact)
+                    .when()
+                    .post("/contacts")
+                    .then()
+                    .statusCode(201)
+                    .extract()
+                    .response();
+
+            ContactDTO addedContact = response.as(ContactDTO.class);
+
+            addressBookList.add(addedContact);
+
+            System.out.println("Added Contact : " + addedContact.getName());
+        }
+
+        System.out.println("Total Contacts in Memory : " + addressBookList.size());
+
+        assertEquals(3,addressBookList.size());
     }
 }
