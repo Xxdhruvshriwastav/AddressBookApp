50e0eea645be012715e6e564c47008d72e48982c
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2026-03-09 02:11:40 +0530

    [Ashish]: added a pojo of ContactDto

---
+++ src/main/java/com/bridgelabz/ContactDTO.java
@@ -0,0 +1,40 @@
+package com.bridgelabz;
+
+public class ContactDTO {
+
+    private int id;
+    private String name;
+    private String city;
+
+    public ContactDTO(){}
+
+    public ContactDTO(int id, String name, String city){
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getCity(){
+        return city;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setCity(String city){
+        this.city = city;
+    }
+}
